9ecd24ddf57b93293c75edf747b318a1a565dbf4
yanghang <silencerfiend@gmail.com>
2018-01-02 18:25:55 +0800

    社会主义好

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
   });  
 
   //核心价值观
+  var a_idx = 0;
   $("body").click(function(e) { 
       var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善"); 
       var $i = $("<span/>").text(a[a_idx]); 
